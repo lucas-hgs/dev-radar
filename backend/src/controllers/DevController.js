1e323e074430c70f5137846487c402efac7dfa94
Lucas Henrique Goncalves da Silveira <lucas.hgs03@gmail.com>
2020-01-20 23:29:03 -0300

    Added socket.io

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
 const parseStringAsArray = require('../utils/parseStringAsArray');
+const { findConnections, sendMessage } = require('../websocket');
 
 module.exports = {
   async update (req, res) {
@@ -77,6 +78,13 @@ module.exports = {
       techs: techsArray,
       location,
     });
+
+    const sendSocketMessageTo = findConnections(
+      { latitude, longitude },
+      techsArray
+    )
+
+    sendMessage(sendSocketMessageTo, 'new-dev', dev);
   
     return res.json(dev);
   }
